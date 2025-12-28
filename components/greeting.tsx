5f367e85a2d5a54f9fe2f0c00a5eec506fe08491
NOIR SOFTWARE <thenoirsoftware@gmail.com>
2025-12-28 06:17:32 +0000

    niiice.

--- components/greeting.tsx
+++ components/greeting.tsx
@@ -22,7 +22,7 @@ export const Greeting = () => {
         initial={{ opacity: 0, y: 10 }}
         transition={{ delay: 0.6 }}
       >
-        How can I help you today?
+        intelligence, without the noise.
       </motion.div>
     </div>
   );
