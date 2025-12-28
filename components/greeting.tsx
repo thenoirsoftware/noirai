b87dd8c715a429237ebc194af7a0ed499143e32c
NOIR SOFTWARE <thenoirsoftware@gmail.com>
2025-12-28 06:14:51 +0000

    FIXED!

--- components/greeting.tsx
+++ components/greeting.tsx
@@ -13,7 +13,7 @@ export const Greeting = () => {
         initial={{ opacity: 0, y: 10 }}
         transition={{ delay: 0.5 }}
       >
-        Hello there!
+        noirAI
       </motion.div>
       <motion.div
         animate={{ opacity: 1, y: 0 }}
